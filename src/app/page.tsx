753797fbdfeabbfe43082147184546d0a752b534
Gokul Alt <l7gokul@gmail.com>
2025-10-14 13:26:11 +0000

    make this in 3 tabs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,8 +6,10 @@ import AddMemberForm from '@/components/AddMemberForm';
 import FamilyTree from '@/components/FamilyTree';
 import NameCardModal from '@/components/NameCardModal';
 import MemberList from '@/components/MemberList';
-import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import { Card, CardContent } from '@/components/ui/card';
 import { ScrollArea } from '@/components/ui/scroll-area';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
+import { Users, UserPlus, GitMerge } from 'lucide-react';
 
 export default function Home() {
   const [selectedMember, setSelectedMember] = useState<TreeNode | null>(null);
@@ -22,25 +24,45 @@ export default function Home() {
 
   return (
     <div className="container mx-auto p-4 sm:p-6 lg:p-8">
-      <div className="grid grid-cols-1 lg:grid-cols-3 gap-8 items-start">
-        <div className="lg:col-span-1 space-y-8">
-          <AddMemberForm />
-          <MemberList />
-        </div>
-
-        <div className="lg:col-span-2">
+      <Tabs defaultValue="tree" className="w-full">
+        <TabsList className="grid w-full grid-cols-3">
+          <TabsTrigger value="tree">
+            <GitMerge className="mr-2 h-4 w-4" />
+            Family Tree
+          </TabsTrigger>
+          <TabsTrigger value="add">
+            <UserPlus className="mr-2 h-4 w-4" />
+            Add Member
+          </TabsTrigger>
+          <TabsTrigger value="list">
+            <Users className="mr-2 h-4 w-4" />
+            Family Members
+          </TabsTrigger>
+        </TabsList>
+        <TabsContent value="tree">
           <Card className="h-full min-h-[80vh]">
-            <CardHeader>
-              <CardTitle className="font-headline text-3xl text-center">Family Tree</CardTitle>
-            </CardHeader>
-            <CardContent>
-              <ScrollArea className="h-[70vh] w-full rounded-md border p-4">
-                 <FamilyTree onNodeClick={handleNodeClick} />
+            <CardContent className="pt-6">
+              <ScrollArea className="h-[75vh] w-full rounded-md border p-4">
+                <FamilyTree onNodeClick={handleNodeClick} />
               </ScrollArea>
             </CardContent>
           </Card>
-        </div>
-      </div>
+        </TabsContent>
+        <TabsContent value="add">
+           <Card className="h-full min-h-[80vh]">
+             <CardContent className="pt-6">
+                <AddMemberForm />
+             </CardContent>
+           </Card>
+        </TabsContent>
+        <TabsContent value="list">
+           <Card className="h-full min-h-[80vh]">
+             <CardContent className="pt-6">
+               <MemberList />
+             </CardContent>
+           </Card>
+        </TabsContent>
+      </Tabs>
 
       {selectedMember && (
         <NameCardModal member={selectedMember} isOpen={!!selectedMember} onClose={handleModalClose} />
